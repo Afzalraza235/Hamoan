c58ec245a9db62eb223de8b0b488582f8642ab1e
MusaZaidi110 <91541474+MusaZaidi110@users.noreply.github.com>
2022-09-19 22:40:12 +0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,14 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then(m => m.LoginModule)
+  },
+  {
+    path: 'signup',
+    loadChildren: () => import('./signup/signup.module').then(m => m.SignupModule)
   }
 ];
 @NgModule({
